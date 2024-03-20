01430566958abcf00c15ad50f74cdca67f6ef6d4
hadibinnoor <hadibinnoor11@gmail.com>
2024-03-20 15:23:43 +0530

    Added org signup and login

--- Events/views.py
+++ Events/views.py
@@ -1,25 +1,37 @@
-from django.shortcuts import get_object_or_404, redirect, render
+# views.py
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Events
 from .forms import EventForm
 
-# Create your views here.
 
 def home(request):
     events = Events.objects.all()
     context = { 'events': events}
-    return render(request, 'hom.html', context)
+    return render(request, 'home.html', context)
 
 def events(request, pk):
     event = get_object_or_404(Events, Event_ID=pk)
     context = {'event': event}
     return render(request, 'events.html', context)
 
+@login_required
 def create_event(request):
     if request.method == 'POST':
         form = EventForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('home')  # Redirect to home page after successful form submission
+            event = form.save(commit=False)
+            # Check if the user is an organization
+            if request.user.Role == 'Org' and hasattr(request.user, 'org_profile'):
+                org = request.user.org_profile
+                event.Created_Org_id = org.Org_ID
+                event.save()
+                return redirect('home')
+            else:
+                # If the user is not an organization or doesn't have an associated organization,
+                # handle the error or redirect as needed
+                return HttpResponse("You are not authorized to create an event.")
     else:
         form = EventForm()
     return render(request, 'create_event.html', {'form': form})
--- Org/models.py
+++ Org/models.py
@@ -1,14 +1,15 @@
+# models.py
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from User.models import User
 
-# Create your models here.
 class Org(models.Model):
-    user =models.OneToOneField(User, related_name = "org_profile", on_delete=models.CASCADE)
-    Org_ID = models.BigAutoField(primary_key=True,unique=True)
-    Org_Name = models.CharField(max_length = 20)
-    Location =models.CharField(max_length=50) 
-    verified = models.BooleanField(default =False)
-    profile=models.ImageField(upload_to="org_profile/",default="")
+    user = models.OneToOneField(User, related_name="org_profile", on_delete=models.CASCADE)
+    Org_ID = models.BigAutoField(primary_key=True, unique=True)
+    Org_Name = models.CharField(max_length=20)
+    Location = models.CharField(max_length=50)
+    verified = models.BooleanField(default=False)
+    profile = models.ImageField(upload_to="org_profile/", default="")
 
     def __str__(self):
         return self.Org_Name
