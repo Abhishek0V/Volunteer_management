d825f30b9c882c7233611ded8dfdafb9e0e3d3f7
hadibinnoor <hadibinnoor11@gmail.com>
2024-03-25 09:04:31 +0530

    Upload Image button added

--- Events/admin.py
+++ Events/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Events,Registered_volunteers
+from .models import Events,Registered_volunteers,Gallery
 # Register your models here.
 
 admin.site.register(Events)
 admin.site.register(Registered_volunteers)
+admin.site.register(Gallery)
